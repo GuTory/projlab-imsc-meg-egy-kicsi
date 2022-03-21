5f34a641e06a28dea8442089598d0df49ccb8a55
fustosgergo <fustosgergo@gmail.com>
2022-03-21 22:42:33 +0100

    Taska, Anyagok attributumok es metodusok hozzaadva

--- VakVirologusok/src/util/Anyagok.java
+++ VakVirologusok/src/util/Anyagok.java
@@ -1,4 +1,17 @@
 package util;
 
+import java.util.Dictionary;
+
 public class Anyagok {
+
+    private Dictionary<String, Integer> anyagok;
+
+    public boolean bevesz(Anyagok anyagok) {
+        return false;
+    }
+
+    public boolean kivesz(Anyagok anyagok) {
+        return false;
+    }
+
 }
--- VakVirologusok/src/util/Taska.java
+++ VakVirologusok/src/util/Taska.java
@@ -1,4 +1,58 @@
 package util;
 
+import agens.Agens;
+import felszereles.Felszereles;
+import lombok.Getter;
+import virologus.Virologus;
+
+import java.util.List;
+
 public class Taska {
+
+    private int maxAnyagok;
+
+    private Anyagok anyagok;
+
+    @Getter
+    private List<Agens> agensek;
+
+    private Virologus virologus;
+
+    @Getter
+    private List<Felszereles> felszerelesek;
+
+
+    public void felszerelesBerak(Felszereles felszereles) {
+
+    }
+
+    public void felszerelesKivesz(Felszereles felszereles) {
+
+    }
+
+    public void anyagBerak(Anyagok anyagok) {
+
+    }
+
+    public Anyagok anyagKivesz(Anyagok anyagok) {
+        return null;
+    }
+
+    public void kapacitasNovel(int ertek) {
+
+    }
+
+    public boolean agensBerak(Agens agens) {
+        return false;
+    }
+
+    public void agensKivesz(Agens agens) {
+
+    }
+
+    public void agensLep() {
+
+    }
+
+
 }
