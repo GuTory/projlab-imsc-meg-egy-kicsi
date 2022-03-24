40c93196ee493f3f7e124647977164f9217bb521
Marcell Balázs Tóth <tothmarcellbalazs@edu.bme.hu>
2022-03-24 17:25:10 +0100

    Felszereles package megírása.

--- VakVirologusok/src/felszereles/Felszereles.java
+++ VakVirologusok/src/felszereles/Felszereles.java
@@ -1,4 +1,27 @@
 package felszereles;
 
-public class Felszereles {
+import jdk.jshell.spi.ExecutionControl;
+import util.Taska;
+import virologus.Virologus;
+
+public abstract class Felszereles{
+    /**
+     * Kifejti hatását az adott virológuson a tárgy, és beteszi a virológus táskájába.
+     * @param virologus, akin kifejti a hatását.
+     * @param taska, ahova berakja a tárgyat.
+     */
+    public abstract void fel(Virologus virologus, Taska taska) throws ExecutionControl.NotImplementedException;
+
+    /**
+     * Elveszi a hatását az adott virológustól, és kiveszi a táskájából.
+     * @param virologus, akitől elveszi a hatását.
+     * @param taska, ahonnan kiveszi a tárgyat.
+     */
+    public abstract void le(Virologus virologus, Taska taska) throws ExecutionControl.NotImplementedException;
+
+    /**
+     * Visszaadja az adott tárgynak az ID-ját, amit a Felszereles_ID osztályból olvas ki.
+     * @return Felszereles_ID
+     */
+    public abstract int getID();
 }
---
+++ VakVirologusok/src/felszereles/Felszereles_ID.java
@@ -0,0 +1,7 @@
+package felszereles;
+
+public class Felszereles_ID {
+    static int kesztyu_ID = 0;
+    static int kopeny_ID = 1;
+    static int zsak_ID = 2;
+}
