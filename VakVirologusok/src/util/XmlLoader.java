b7f4add22b70bbff224b8617138f556ccb540a57
Balint Beke <bekebalint98@gmail.com>
2022-04-23 13:22:49 +0200

    Osztalyok

---
+++ VakVirologusok/src/util/XmlLoader.java
@@ -0,0 +1,12 @@
+package util;
+import jatek.Varos;
+
+import java.io.IOException;
+
+public class XmlLoader 
+{
+    public static Varos load(String fileName) throws IOException
+    {
+        return null;
+    }
+}
---
+++ VakVirologusok/src/util/XmlSaver.java
@@ -0,0 +1,12 @@
+package util;
+import jatek.Varos;
+
+import java.io.IOException;
+
+public class XmlSaver
+{
+    public static void save(String fileName, Varos state) throws IOException
+    {
+
+    }
+}
