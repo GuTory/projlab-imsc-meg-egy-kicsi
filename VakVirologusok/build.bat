49df6cad36ee25874a87ae73b2b3211b8cf1a802
tmb2k01 <marcsello.ma@gmaio.com>
2022-04-23 22:13:27 +0200

    build.bat

--- VakVirologusok/build.bat
+++ VakVirologusok/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-javac src/*.java src/felszereles/*.java src/agens/*.java src/jatek/*.java src/Serialization/*.java src/terkep/*.java src/test/*.java src/util/*.java src/virologus/*.java src/skeleton/*.java -encoding UTF-8 -d out
+javac src/*.java src/felszereles/*.java src/agens/*.java src/jatek/*.java src/Serialization/*.java src/terkep/*.java src/test/*.java src/util/*.java src/virologus/*.java -encoding UTF-8 -d out
 
 cd out
 
