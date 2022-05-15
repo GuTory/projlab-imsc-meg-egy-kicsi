bcd7acc8aed53f6cc06abfee9f085b0f1a440be6
Bálint Beke <bekebalint98@gmail.com>
2022-05-15 15:11:57 +0200

    build fix

--- VakVirologusok/build.bat
+++ VakVirologusok/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-javac src/*.java src/*/*.java src/*/*/*.java -encoding UTF-8 -d out
+javac src/*.java src/agens/*.java src/felszereles/*.java src/Graphics/*.java src/jatek/*.java src/terkep/*.java src/test/*.java src/util/*.java src/Graphics/ActionFrames/*.java src/virologus/*.java -encoding UTF-8 -d out
 
 cd out
 
