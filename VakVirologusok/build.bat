4ad712870f4c3d158fe8feb173c4e32bfb3cdb5e
Bálint Beke <bekebalint98@gmail.com>
2022-05-15 14:21:47 +0200

    Build script, skeleton cuccok torolve

--- VakVirologusok/build.bat
+++ VakVirologusok/build.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-javac src/*.java src/felszereles/*.java src/agens/*.java src/jatek/*.java src/Serialization/*.java src/terkep/*.java src/test/*.java src/util/*.java src/virologus/*.java -encoding UTF-8 -d out
+javac src/*.java src/*/*.java src/*/*/*.java -encoding UTF-8 -d out
 
 cd out
 
-java ProtoProgram
+java MainProgram
 
 cd..
